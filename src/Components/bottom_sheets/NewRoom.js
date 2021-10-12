f51a91e1d21bd3639a295c570df7f2491b015175
muhammedAnjoom <anjoompa42003@gmail.com>
2021-10-12 07:31:00 +0530

    new room bottom nagvigators

--- src/Components/bottom_sheets/NewRoom.js
+++ src/Components/bottom_sheets/NewRoom.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import style from "../../style/roomDetail.module.css";
-import { AiOutlineFile } from "react-icons/ai";
-import { BsMicMuteFill } from "react-icons/bs";
+import { AiOutlineFile, AiOutlinePlus } from "react-icons/ai";
+import { BsMicMuteFill, BsMicFill } from "react-icons/bs";
 
 function NewRoom(props) {
-  const [micMuteVisible, setmicMuteVisible] = useState(true);
+  const [micMuteVisible, setmicMuteVisible] = useState(false);
+  const [itemsVisible, setItemsVisible] = useState(true);
   const card = props.cardDetail;
   return (
     <>
@@ -32,23 +33,49 @@ function NewRoom(props) {
         </div>
         <div className={style.roomDetailcard}>
           <div
-           className="d-flex align-items-center justify-content-between"
+            className="d-flex align-items-center justify-content-between"
             style={{ padding: "0.5em 1em" }}
           >
-              {card.members.map((item) => (
-                  <div className={style.membersContanier}>
-                      {micMuteVisible ? (
-                        <div className={style.audio_icon}>
-                            <BsMicMuteFill/>
-                        </div>   
-                      ) : ("")}
-                      <img src="/images/myimage.png" alt="" />
-                      <p>
-                          <span>*</span>
-                          {item.first_name}
-                      </p>
+            {card.members.map((item) => (
+              <div className={style.membersContanier}>
+                {micMuteVisible ? (
+                  <div className={style.audio_icon}>
+                    <BsMicMuteFill />
                   </div>
-              ))}
+                ) : (
+                  ""
+                )}
+                <img src="/images/myimage.png" alt="" />
+                <p>
+                  <span>*</span>
+                  {item.first_name}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className={style.foote}>
+          <button
+            onClick={() => {
+              props.setSheetVisible(false);
+            }}
+          >
+            <img src="/images/hand-peace.png" alt="" />
+            Leave Quietl
+          </button>
+          <div>
+            <button>
+              <AiOutlinePlus/>
+            </button>
+            <button>
+              <img src="/images/stopHandIcon.png" alt="" />
+            </button>
+            <button 
+            onClick={()=>{
+              setmicMuteVisible(!micMuteVisible)
+            }}>
+              {micMuteVisible ? <BsMicMuteFill/> : <BsMicFill/>}
+            </button>
           </div>
         </div>
       </div>
